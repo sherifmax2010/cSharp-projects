e959a3520840ea7c1c5775ae0a9b6a0a59a92164
sherifmax2010 <sherifmax2010@users.noreply.github.com>
2025-08-12 10:47:28 +0300

    add new code The Point

---
+++ ConsoleApp1/ConsoleApp1/The Point.cs
@@ -0,0 +1,20 @@
+﻿Point point1 = new Point(2, 3);
+Point point2 = new Point(-4, 0);
+Console.WriteLine("("+ point1.x + "," + point1.y +")" );
+Console.WriteLine("(" + point2.x + "," + point2.y + ")");
+
+class Point
+{
+    public int x { get; }
+    public int y { get; }
+    public  Point(int x, int y)
+    {
+        this .x = x;
+        this .y = y;
+    }
+    public  Point() : this(0,0)
+    {
+        return x = y = 0;
+    }
+
+}
