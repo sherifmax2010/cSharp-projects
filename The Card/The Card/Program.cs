fbae4e5f8bfdfc0371998168f9d2e2a6f216f146
sherifmax2010 <sherifmax2010@users.noreply.github.com>
2025-08-12 12:39:04 +0300

    the new code TheCard

---
+++ The Card/The Card/Program.cs
@@ -0,0 +1,35 @@
+﻿
+enum color
+{
+    red,
+    green,
+    blue,
+    yellow
+}
+enum rank
+{
+    one = 1,
+    two = 2,
+    three = 3,
+    four = 4,
+    five = 5,
+    six = 6,
+    seven = 7,
+    eight = 8,
+    nine = 9,
+    ten = 10,
+    Dollar,
+    Percent,
+    Caret,
+    Ampersand
+}
+class Card
+{
+    public string color;
+    public string rank;
+    public Card(string Color, string rank)
+    {
+        this.color = color;
+        this.rank = rank;
+    }
+}
